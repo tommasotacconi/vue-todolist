38c0c9269ff87fd308adac0e249fa16f0479ad2f
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-05 17:20:14 +0200

    completed milestone 1

--- jscript/script.js
+++ jscript/script.js
@@ -4,9 +4,79 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        toDoList: [
+          {
+            text: 'Comprare una bella tastiera',
+            done: false,
+          },
+          {
+            text: 'Allenamento delle 18.30',
+            done: false,
+          },
+          {
+            text: 'Fare degli esercizi di mobilità',
+            done: true,
+          },
+          {
+            text: 'Capire meglio il CSS',
+            done: false,
+          },
+          {
+            text: 'Fare l\'esercizio del giorno per tempo',
+            done: false,
+          },
+          {
+            text: 'Passare del tempo all\'aperto',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css, esempio di testo molto lungo scritto all\'interno di questo li e potrei continuare a scrivere',
+            done: false,
+          },
+          {
+            text: 'Essere più veloci con il css',
+            done: false,
+          },
+        ],
       }
+    },
+    methods: {
+      getItemText: () => this.toDoList.text,
     }
   }).mount('#app')
+
+
 
 
