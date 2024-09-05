3d67cad908d873ee0e777cddb17a5ac9d177bfce
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-09-05 20:48:35 +0200

    added milestone 3

--- jscript/script.js
+++ jscript/script.js
@@ -57,30 +57,28 @@ const { createApp } = Vue
             text: 'Allenarsi ancora con il css',
             done: false,
           },
-          {
-            text: 'Allenarsi ancora con il css',
-            done: false,
-          },
-          {
-            text: 'Allenarsi ancora con il css',
-            done: false,
-          },
-          {
-            text: 'Allenarsi ancora con il css',
-            done: false,
-          },
         ],
+        isInputVisible: false,
+        addedText: '',
       }
     },
     methods: {
       deleteItem (elementIndex) {
+        // Rimuovo con splice il solo elemento con indice dato in input alla funzione
         this.toDoList.splice(elementIndex, 1);
+        // Stampo in console l'operazione precedente
         console.log(`E' stato eliminato l'emento d'array con indice ${elementIndex}`);
       },
-
-    }
-
-
+      toggleInput () {
+        this.isInputVisible = !this.isInputVisible;
+      },
+      addToDo (text) {
+        // Creo un oggetto letterale con proprietà 'text' pari a quella data come input e con proprietà 'done' settata a false
+        const newObj = {text, done: false};
+        this.toDoList.push(newObj);
+        this.addedText = '';
+      }
+    },
   }).mount('#app')
 
 
